1e309fb1f52be3a9217e0de21526726d8c1b8749
YevhenBrovchuk <brovchukyevhen@ukr.net>
2024-01-27 10:17:29 +0200

    Slice

--- src/components/CarsList/CarsList.js
+++ src/components/CarsList/CarsList.js
@@ -1,7 +1,11 @@
 import { CarCard } from 'components/CarCard/CarCard'
 import { useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { incrementfavoritCars, incrementCar } from '../../redux/store'
+import {
+	incrementfavoritCars,
+	incrementCar,
+	dicrementfavoritCars,
+} from '../../redux/store'
 
 export const CarsList = () => {
 	const dispatch = useDispatch()
@@ -9,7 +13,7 @@ export const CarsList = () => {
 
 	const listCars = useSelector(state => state.listCars)
 	const favoritlistcars = useSelector(state => state.listCarsfavorit)
-
+	console.log(favoritlistcars.length)
 	let qqq
 	let flag = false
 	if (pathname === '/catalog') {
@@ -19,13 +23,28 @@ export const CarsList = () => {
 	if (pathname === '/favorites') {
 		qqq = favoritlistcars
 	}
+
+	// const flagFavoritCars = () => {
+	// 	if (favoritlistcars.length > 0) {
+	// 		return dicrementfavoritCars
+	// 	}
+	// 	return incrementfavoritCars
+	// }
+	// console.log(flagFavoritCars())
 	return (
 		<>
 			<div>{pathname}</div>
 			<ul>
 				{qqq.map(({ id, name }) => (
 					<li key={id}>
-						{flag && (
+						{flag && favoritlistcars.length > 0 ? (
+							<button
+								type='button'
+								onClick={() => dispatch(dicrementfavoritCars({ id, name }))}
+							>
+								{name}
+							</button>
+						) : (
 							<button
 								type='button'
 								onClick={() => dispatch(incrementfavoritCars({ id, name }))}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit'
 
 const cars = [
 	{
@@ -15,29 +15,65 @@ const cars = [
 		name: 'ford',
 	},
 ]
-export const incrementCar = createAction('incrementCar')
+
+const listCarsSlice = createSlice({
+	name: 'listCars',
+	initialState: cars,
+	reducers: {
+		incrementCar(state, action) {
+			state.push(action.payload)
+		},
+	},
+})
+
+export const { incrementCar } = listCarsSlice.actions
+export default listCarsSlice.reducer
+
+const listCarsfavoritSlice = createSlice({
+	name: 'listCarsfavorit',
+	initialState: [],
+	reducers: {
+		incrementfavoritCars(state, action) {
+			state.push(action.payload)
+		},
+		dicrementfavoritCars(state, action) {
+			state.filter(item => item !== action.payload)
+		},
+	},
+})
+
+export const { incrementfavoritCars, dicrementfavoritCars } =
+	listCarsfavoritSlice.actions
+// export const incrementCar = createAction('incrementCar')
 // console.log(incrementCar())
-export const incrementfavoritCars = createAction(
-	'listCarsfavorit/incrementfavoritCars'
-)
+// export const incrementfavoritCars = createAction(
+// 	'listCarsfavorit/incrementfavoritCars'
+// )
+// export const dicrementfavoritCars = createAction(
+// 	'listCarsfavorit/dicrementfavoritCars'
+// )
 
-const carListReducer = createReducer(cars, builder => {
-	// console.log('HELP')
-	builder.addCase(incrementCar, (state, action) => {
-		console.log(action.payload)
-		state.push(action.payload)
-	})
-})
+// const carListReducer = createReducer(cars, builder => {
+// 	// console.log('HELP')
+// 	builder.addCase(incrementCar, (state, action) => {
+// 		console.log(action.payload)
+// 		state.push(action.payload)
+// 	})
+// })
 
-const favoritCarsReducer = createReducer([], builder => {
-	builder.addCase(incrementfavoritCars, (state, action) => {
-		state.push(action.payload)
-	})
-})
+// const favoritCarsReducer = createReducer([], builder => {
+// 	builder
+// 		.addCase(incrementfavoritCars, (state, action) => {
+// 			state.push(action.payload)
+// 		})
+// 		.addCase(dicrementfavoritCars, (state, action) => {
+// 			state.filter(item => item !== action.payload)
+// 		})
+// })
 
 export const store = configureStore({
 	reducer: {
-		listCars: carListReducer,
-		listCarsfavorit: favoritCarsReducer,
+		listCars: listCarsSlice.reducer,
+		listCarsfavorit: listCarsfavoritSlice.reducer,
 	},
 })
