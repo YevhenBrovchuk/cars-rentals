5fcd6d6ce8a471d2a9245a0934045c5f7cc5c407
YevhenBrovchuk <brovchukyevhen@ukr.net>
2024-02-03 11:02:27 +0200

    http request

--- src/redux/operations/carsOperations.js
+++ src/redux/operations/carsOperations.js
@@ -1,32 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-// import { fetchCars } from 'services/listcars-api'
+
 import axios from 'axios'
 
-// export const fetchCarsList = createAsyncThunk(
-// 	'listCars/fetchCars',
-// 	async ({ page, limit }, { rejectWithValue }) => {
-// 		try {
-// 			console.log(page, limit)
-// 			const cars = await fetchCars(page, limit)
-// 			return cars
-// 		} catch (error) {
-// 			return rejectWithValue(error)
-// 		}
-// 	}
-// )
-
-axios.defaults.baseURL = 'http://65abbecffcd1c9dcffc6f56e.mockapi.io'
+axios.defaults.baseURL = 'https://65bcd7ccb51f9b29e93266d4.mockapi.io'
 const params = new URLSearchParams({
 	page: 1,
-	limit: 12,
+	limit: 5,
 })
 
 export const getAll = createAsyncThunk(
-	'advert/fatchAll',
+	'listCars/fetchCars',
 	async (_, { rejectWithValue }) => {
 		try {
 			console.log(params)
-			const { data } = await axios.get(`/advert?`)
+			const { data } = await axios.get(`/advert`)
 			return data
 		} catch (error) {
 			return rejectWithValue(error.message)
